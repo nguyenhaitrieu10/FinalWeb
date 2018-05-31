fee4bbd62520e0677068565b7f9ac8fc3caac0ea
trieu <nguyenhaitrieu10@gmail.com>
2018-06-01 00:08:29 +0700

    admin

--- routes/index.js
+++ routes/index.js
@@ -34,4 +34,32 @@ router.get('/update-info', function(req, res, next) {
   res.render('update-info', { title: 'Express' });
 });
 
+router.get('/admin-home', function(req, res, next) {
+  res.render('admin-home', { title: 'Express', layout: "admin-layout" });
+});
+
+router.get('/admin-login', function(req, res, next) {
+  res.render('admin-login', { title: 'Express', layout: null});
+});
+
+router.get('/admin-order-detail', function(req, res, next) {
+  res.render('admin-order-detail', { title: 'Express', layout: "admin-layout" });
+});
+
+router.get('/admin-order', function(req, res, next) {
+  res.render('admin-order', { title: 'Express', layout: "admin-layout" });
+});
+
+router.get('/admin-product', function(req, res, next) {
+  res.render('admin-product', { title: 'Express', layout: "admin-layout" });
+});
+
+router.get('/admin-store', function(req, res, next) {
+  res.render('admin-store', { title: 'Express', layout: "admin-layout" });
+});
+
+router.get('/admin-user', function(req, res, next) {
+  res.render('admin-user', { title: 'Express', layout: "admin-layout" });
+});
+
 module.exports = router;
