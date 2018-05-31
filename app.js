fb1470f9c2634af80684425788f93f3d8c5d3d3b
trieu <nguyenhaitrieu10@gmail.com>
2018-05-31 20:48:57 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var exphbs = require('express-handlebars');
+var models = require('./models');
+var paginateHelper = require('express-handlebars-paginate');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var hbs = exphbs.create({
+	defaultLayout: 'layout',
+	extname: 'hbs',
+	layoutsDir: __dirname + '/views/layouts/',
+	partialsDir: __dirname + '/views/partials',
+	helpers: {
+		paginate: paginateHelper.createPagination
+	}
+});
+
+// app.set('views', path.join(__dirname, 'views'));
+app.engine('hbs',hbs.engine);
+app.set('view engine', 'hbs');
+app.set('port',(process.env.PORT || 3000));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Router
+var indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+app.listen(app.get('port'), function(){
+	console.log('Server is listening at port ' + app.get('port'));
+});
+
+module.exports = app;
