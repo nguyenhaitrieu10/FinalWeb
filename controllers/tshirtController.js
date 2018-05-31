9ad9ab27c194b33dc7b38e79cfd34e725f873b86
trieu <nguyenhaitrieu10@gmail.com>
2018-05-31 20:52:18 +0700

    edit

---
+++ controllers/tshirtController.js
@@ -0,0 +1,24 @@
+// var controller  = {};
+
+// var models = require('../models');
+
+// controller.getAll = function(callback){
+// 	models.article
+// 	.findAll()
+// 	.then(function(articles){
+// 		callback(articles);
+// 	});
+// }
+
+// controller.getById = function(id, callback){
+// 	models.article
+// 	.findOne({
+// 		where: {id: id}
+// 		// include: [models.comment]
+// 	})
+// 	.then(function(article){
+// 		callback(article);
+// 	});
+// }
+
+// module.exports = controller;
